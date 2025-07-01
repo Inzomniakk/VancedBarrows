312bda4461924ff193d2f5b868c7314d1e243c68
Inzomniakk <inzomniaktv@gmail.com>
2025-07-01 16:21:46 -0400

    Filenames changed.

--- src/main/java/com/VancedBarrows/VancedBarrowsPlugin.java
+++ src/main/java/com/VancedBarrows/VancedBarrowsPlugin.java
@@ -14,7 +14,7 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Vanced Barrows"
 )
 public class VancedBarrowsPlugin extends Plugin
 {
