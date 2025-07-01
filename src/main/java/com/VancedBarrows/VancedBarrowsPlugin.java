2061012f3ce86606f9fe1d0b09f03f85add30b94
Inzomniakk <inzomniaktv@gmail.com>
2025-07-01 19:59:38 -0400

    Adds the vance png overlay, need to adjust size and coloring. Then work on loading only near barrows.

--- src/main/java/com/VancedBarrows/VancedBarrowsPlugin.java
+++ src/main/java/com/VancedBarrows/VancedBarrowsPlugin.java
@@ -2,7 +2,6 @@ package com.VancedBarrows;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
-import javax.inject.Singleton;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
 import net.runelite.api.GameState;
